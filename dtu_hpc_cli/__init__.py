847cb8d869dff7c685f1a83b16d0325910a1975a
Christoffer Koo Øhrstrøm <2357447+ChrisFugl@users.noreply.github.com>
2024-09-07 10:03:15 +0000

    Setup cli

---
+++ dtu_hpc_cli/__init__.py
@@ -0,0 +1,23 @@
+import typer
+
+
+cli = typer.Typer()
+
+
+@cli.command()
+def list():
+    print("list")
+
+
+@cli.command()
+def run():
+    print("run")
+
+
+@cli.command()
+def submit():
+    print("submit")
+
+
+if __name__ == "__main__":
+    cli()
