2646357d4afa85f5a611d86976b93f87cff111bf
Christoffer Koo Øhrstrøm <2357447+ChrisFugl@users.noreply.github.com>
2024-10-05 16:29:32 +0000

    Add remove command

---
+++ dtu_hpc_cli/remove.py
@@ -0,0 +1,9 @@
+from dtu_hpc_cli.client import get_client
+from dtu_hpc_cli.config import Config
+
+
+def execute_remove(cli_config: Config, job_ids: list[str]):
+    with get_client(cli_config) as client:
+        for job_id in job_ids:
+            output = client.run(f"bkill {job_id}")
+            print(output)
