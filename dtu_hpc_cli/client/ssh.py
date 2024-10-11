14248d3f9a9fc396e8000d12b33374ec6292e1e4
Christoffer Koo Øhrstrøm <2357447+ChrisFugl@users.noreply.github.com>
2024-10-11 13:08:46 +0000

    Add local client

--- dtu_hpc_cli/client/ssh.py
+++ dtu_hpc_cli/client/ssh.py
@@ -30,15 +30,6 @@ class SSHClient(Client):
 
         self.num_prompt_lines = self.get_num_prompt_lines()
 
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close()
-
-    def __del__(self):
-        self.close()
-
     def close(self):
         sftp = getattr(self, "sftp", None)
         if sftp is not None:
