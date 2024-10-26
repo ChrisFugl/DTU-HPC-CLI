2e3810c387bc824317d05bfb7c142c2bf6ca35de
Christoffer Koo Øhrstrøm <2357447+ChrisFugl@users.noreply.github.com>
2024-10-26 13:11:20 +0000

    Cleanup

--- dtu_hpc_cli/error.py
+++ dtu_hpc_cli/error.py
@@ -4,7 +4,6 @@ import rich
 
 
 def error_and_exit(message: str, code: int = 1):
-    # typer.secho(message, fg=typer.colors.RED, bold=True)
     panel = rich.panel.Panel(message, border_style="red", title="Error", title_align="left")
     rich.print(panel)
     sys.exit(code)
